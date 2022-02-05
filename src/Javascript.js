e91af5712f7de7d839711dfb83976e67536b7971
shiv270 <shivprakashyadav@cedcoss.com>
2022-02-05 11:45:43 +0530

    Subtract One Number from Another with JavaScript

--- src/Javascript.js
+++ src/Javascript.js
@@ -64,3 +64,5 @@ fact = "is awesome!";
 console.log(FCC, fact); // Change this line
 //Add Two Numbers with JavaScript
 const sum = 10 + 10;
+//Subtract One Number from Another with JavaScript
+const difference = 45 -33;
