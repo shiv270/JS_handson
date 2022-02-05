0ab15a148805467e46b3b9013f26e0fc9184f303
shiv270 <shivprakashyadav@cedcoss.com>
2022-02-05 12:46:20 +0530

    Find the Length of a String

--- src/Javascript.js
+++ src/Javascript.js
@@ -159,3 +159,12 @@ const myStr = "Shiv" + myName+ "Yadav";
 const someAdjective = "Awesome";
 let myStr = "Learning to code is ";
 myStr+=someAdjective
+
+//Find the Length of a String
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName;
+lastNameLength=lastName.length;
