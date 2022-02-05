bc43cb7ee7ef0f4f84b932f09f4de207c98b96bb
shiv270 <shivprakashyadav@cedcoss.com>
2022-02-05 14:40:23 +0530

    Comparison with the Equality Operator

--- src/Javascript.js
+++ src/Javascript.js
@@ -465,3 +465,13 @@ function welcomeToBooleans() {
   
   // Change this value to test
   trueOrFalse(true);
+//Comparison with the Equality Operator
+  // Setup
+function testEqual(val) {
+    if (val==12) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  testEqual(10);
