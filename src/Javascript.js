88f05d2134824868ba6955b1cc7173a66364b191
shiv270 <shivprakashyadav@cedcoss.com>
2022-02-05 11:25:41 +0530

    Understanding Uninitialized Variables

--- src/Javascript.js
+++ src/Javascript.js
@@ -28,3 +28,16 @@ var a=9;
 
 var myFirstName='Shiv'
 var myLastName='Yadav'
+
+//Understanding Uninitialized Variables
+// Only change code below this line
+var a;
+var b;
+var c;
+// Only change code above this line
+a=5;
+b=10;
+c="I am a"
+a = a + 1;
+b = b + 5;
+c = c + " String!";
