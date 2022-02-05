0b4b2fc5d7287951f45551122a5f9f0da67255a2
shiv270 <shivprakashyadav@cedcoss.com>
2022-02-05 12:23:51 +0530

    Quoting Strings with Single Quotes

--- src/Javascript.js
+++ src/Javascript.js
@@ -136,3 +136,6 @@ c /=11;
 
 //Escaping Literal Quotes in Strings
 const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+//Quoting Strings with Single Quotes
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
