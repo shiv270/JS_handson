453cd10e2c467fed645ae58d50b7187defbf6542
shiv270 <shivprakashyadav@cedcoss.com>
2022-02-05 14:53:59 +0530

    Comparison with the Less Than Operator

--- src/Javascript.js
+++ src/Javascript.js
@@ -601,5 +601,19 @@ function testStrictNotEqual(val) {
   
     return "Less than 10";
   }
-  
+  //Comparison with the Less Than Operator
   testGreaterOrEqual(10);
+
+  function testLessThan(val) {
+    if (val<25) {  // Change this line
+      return "Under 25";
+    }
+  
+    if (val<55) {  // Change this line
+      return "Under 55";
+    }
+  
+    return "55 or Over";
+  }
+  
+  testLessThan(10);
