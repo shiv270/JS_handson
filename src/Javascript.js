cea70950822d2b31a64c5dcf19cb6042bd8d971e
shiv270 <shivprakashyadav@cedcoss.com>
2022-02-05 14:44:49 +0530

    Comparison with the Inequality Operator

--- src/Javascript.js
+++ src/Javascript.js
@@ -497,3 +497,14 @@ function compareEquality(a, b) {
   }
   
   compareEquality(10, "10");
+
+//Comparison with the Inequality Operator
+  // Setup
+function testNotEqual(val) {
+    if (val!=99) { // Change this line
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  testNotEqual(10);
