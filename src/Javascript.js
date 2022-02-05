eb64a00905d07e280cc0a5455d9871431a996c20
shiv270 <shivprakashyadav@cedcoss.com>
2022-02-05 13:55:34 +0530

    Global Scope and Functions

--- src/Javascript.js
+++ src/Javascript.js
@@ -297,3 +297,28 @@ function reusableFunction(){
     return num*5
   }
   timesFive()
+
+//Global Scope and Functions
+
+  // Declare the myGlobal variable below this line
+
+
+function fun1() {
+    oopsGlobal=5;
+    // Assign 5 to oopsGlobal Here
+  
+  }
+  let myGlobal=10;
+  
+  // Only change code above this line
+  
+  function fun2() {
+    var output = "";
+    if (typeof myGlobal != "undefined") {
+      output += "myGlobal: " + myGlobal;
+    }
+    if (typeof oopsGlobal != "undefined") {
+      output += " oopsGlobal: " + oopsGlobal;
+    }
+    console.log(output);
+  }
