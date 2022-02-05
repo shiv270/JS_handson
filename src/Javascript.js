6646122f4390ebdcd9f5e25d71db7b4099c4bffd
shiv270 <shivprakashyadav@cedcoss.com>
2022-02-05 12:58:26 +0530

    Use Bracket Notation to Find the Nth Character in a String

--- src/Javascript.js
+++ src/Javascript.js
@@ -185,3 +185,11 @@ let myStr = "Jello World";
 // Only change code below this line
 myStr = "Hello World"; // Change this line
 // Only change code above this line
+
+//Use Bracket Notation to Find the Nth Character in a String
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
