7082ebbbe4e815c3ec297912afad6e24fed0166e
shiv270 <shivprakashyadav@cedcoss.com>
2022-02-05 12:35:06 +0530

    Concatenating Strings with Plus Operator

--- src/Javascript.js
+++ src/Javascript.js
@@ -142,3 +142,6 @@ const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
 
 //Escape Sequences in Strings
 const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+//Concatenating Strings with Plus Operator
+const myStr = "This is the start." + " This is the end."; // Change this line
