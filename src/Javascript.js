a2e1e3cd66d4172a6f61048e8993095d2b483c64
shiv270 <shivprakashyadav@cedcoss.com>
2022-02-05 13:33:44 +0530

    Manipulate Arrays With pop()

--- src/Javascript.js
+++ src/Javascript.js
@@ -254,4 +254,12 @@ const myArray = [["John", 23], ["cat", 2]];
 // Only change code below this line
 myArray.push(["dog",3])
 
+//Manipulate Arrays With pop()
+
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+const removedFromMyArray=myArray.pop(["John",33])
+
 
