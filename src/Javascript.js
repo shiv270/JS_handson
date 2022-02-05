30efae06e63111b46f9a26fb54b5eb9cafc536a4
shiv270 <shivprakashyadav@cedcoss.com>
2022-02-05 15:28:05 +0530

    Returning Boolean Values from Functions

--- src/Javascript.js
+++ src/Javascript.js
@@ -881,3 +881,12 @@ function chainToSwitch(val) {
   
   // Change this value to test
   chainToSwitch(7);
+
+  //Returning Boolean Values from Functions
+  function isLess(a, b) {
+    // Fix this code
+    return a < b;
+  }
+  
+  // Change these values to test
+  isLess(10, 15);
