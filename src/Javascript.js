f4dc10911c2533343f458b6f372cf29a91d0be9d
shiv270 <shivprakashyadav@cedcoss.com>
2022-02-05 15:24:09 +0530

    Adding a Default Option in Switch Statements

--- src/Javascript.js
+++ src/Javascript.js
@@ -788,6 +788,33 @@ function testSize(num) {
         answer = "delta";
         break;
     }
+
+    function switchOfStuff(val) {
+        var answer = "";
+        // Only change code below this line
+      
+        switch (val) {
+          case "a":
+            answer = "apple";
+            break;
+          case "b":
+            answer = "bird";
+            break;
+          case "c":
+            answer = "cat";
+            break;
+          default:
+            answer = "stuff";
+        }
+      
+      
+        // Only change code above this line
+        return answer;
+      }
+      
+    //Adding a Default Option in Switch Statements
+      // Change this value to test
+      switchOfStuff(1);
   
   
     // Only change code above this line
