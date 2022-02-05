5ba42cd5586e69e9f4310d01b0f75a834420e3bb
shiv270 <shivprakashyadav@cedcoss.com>
2022-02-05 14:29:29 +0530

    function nextInLine(arr, item) {

--- src/Javascript.js
+++ src/Javascript.js
@@ -410,3 +410,23 @@ function processArg(num) {
 
 // Only change code below this line
 processed = processArg(7);
+
+//stand in Line
+
+function nextInLine(arr, item) {
+    // Your code here
+  
+    var queue = arr.push(item);
+  
+    var removeItem = arr.shift();
+  
+    return removeItem;  // Change this line
+  }
+  
+  // Test Setup
+  var testArr = [1,2,3,4,5];
+  
+  // Display Code
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6)); // Modify this line to test
+  console.log("After: " + JSON.stringify(testArr)); 
